dace73c233afcc2073f6d2a873f2d2c328298ce0
gurkansuna <74645573+gurkansuna@users.noreply.github.com>
2026-09-21 15:57:44 +0300

    Remove unused files

--- nuxt-app/server/lib/domainError.ts
+++
@@ -1,3 +0,0 @@
-// Kullanıcıya doğrudan gösterilmesi güvenli, Türkçe hata mesajı taşıyan istisna.
-// Orijinal server.js'deki her `throw new Error("...")` çağrısının karşılığı.
-export class DomainError extends Error {}
